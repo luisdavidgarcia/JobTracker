4d48e0f45506122ebbe42786e80b5f466b123c04
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-01-15 02:22:22 -0800

    Added battle plan

--- job_tracker/main.py
+++ job_tracker/main.py
@@ -14,3 +14,20 @@ chain = prompt | model
 response = chain.invoke({"question": "What is LangChain?"})
 
 print(response)
+
+# Tasks to do (Get it Done first then iterate)
+'''
+Input: Copy and pasted description of job just from clipboard 
+Output: Added job entry to database
+
+Always been a CLI warrior will just make a CLI for this
+
+Steps:
+1. Create a means of just acqurign the copied job description from clipboard
+2. Using langchain make it analyze the description and put it in format that can be added to database
+3. Utilize the schema you have to help with context for model
+4. Have Ollama model attempt to create the job entry as a SQL query
+5. Add a verification step to allow the user to view and confirm the job entry
+5. Add the job entry to the database
+6. Return a success message to the user
+'''
